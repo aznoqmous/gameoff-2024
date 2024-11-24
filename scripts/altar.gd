affe0a26801f80a22565713cfbd5140559e56d3c
aznoqmous <paul.landreau931v@gmail.com>
2024-11-24 11:37:16 +0100

    demo

--- scripts/altar.gd
+++ scripts/altar.gd
@@ -1,12 +1,22 @@
 extends Node2D
 
 @onready var area_2d: Area2D = $Area2D
-@onready var game: Game = $"../.."
+@onready var game: Game = $".."
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@export var symbol : AltarSymbol
 
 func _ready() -> void:
-	area_2d.area_entered.connect(teleport)
+	area_2d.area_entered.connect(play_teleport)
 
-func teleport(area: Area2D):
-	print(game.player)
+func reset_animation():
+	animation_player.play("idle")
+	
+func play_teleport(area: Area2D):
+	animation_player.play("teleport")
+
+func teleport():
 	game.player.targetPositions.clear()
 	game.player.set_current_position(game.sanctuary.position)
+
+func activate_symbol():
+	if symbol: symbol.activate()
---
+++ scripts/altar_symbol.gd
@@ -0,0 +1,23 @@
+class_name AltarSymbol
+extends Node2D
+
+@onready var point_light_2d: PointLight2D = $PointLight2D
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+var state = false
+
+func _ready():
+	sprite_2d.modulate.a = 0
+	point_light_2d.energy = 0
+	sprite_2d.position = Vector2.UP * 100
+
+func _process(delta: float) -> void:
+	if state: 
+		sprite_2d.modulate.a = lerp(sprite_2d.modulate.a, 1.0, delta)
+		point_light_2d.energy = lerp(point_light_2d.energy, 0.2, delta)
+		sprite_2d.position = sprite_2d.position.lerp(Vector2.ZERO, delta)
+func is_active():
+	return state
+
+func activate():
+	state = true
