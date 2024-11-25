4e3af203499b19bcac86e2e8a88470813343db5c
aznoqmous <paul.landreau931v@gmail.com>
2024-11-25 22:09:14 +0100

    started scene progress load

--- scripts/managers/scene_manager.gd
+++ scripts/managers/scene_manager.gd
@@ -1,35 +1,54 @@
-extends Node
+extends CanvasLayer
 
 var current_scene = null
 var game_scene = "res://scenes/game.tscn"
 var title_scene = "res://scenes/title.tscn"
+var pre_scene_manager_progress = preload("res://scenes/scene_manager_progress.tscn")
+var scene_manager_progress: SceneManagerProgress
 
 func _ready():
 	var root = get_tree().root
 	current_scene = root.get_child(root.get_child_count() - 1)
 	load_title()
+	
+	scene_manager_progress = pre_scene_manager_progress.instantiate()
+	add_child(scene_manager_progress)
 
 func load_title():
 	load_scene(title_scene)
 	
 func load_game():
-	load_scene(game_scene)
+	load_scene_progress(game_scene)
 
 func load_scene(path):
 	_deferred_goto_scene.call_deferred(path)
 
 func _deferred_goto_scene(path):
-	# It is now safe to remove the current scene.
-	current_scene.free()
-
-	# Load the new scene.
 	var s = ResourceLoader.load(path)
-
-	# Instance the new scene.
-	current_scene = s.instantiate()
-
-	# Add it to the active scene, as child of root.
+	set_scene(s.instantiate())
+	
+func set_scene(scene):
+	current_scene.free()
+	current_scene = scene
 	get_tree().root.add_child(current_scene)
-
-	# Optionally, to make it compatible with the SceneTree.change_scene_to_file() API.
 	get_tree().current_scene = current_scene
+
+func load_scene_progress(path):
+	scene_manager_progress.set_progress(0.0)
+	await scene_manager_progress.appear()
+	var s = await _thread_load(path)
+	set_scene(s.instantiate())
+	await scene_manager_progress.disappear()
+	
+func _thread_load(path):
+	ResourceLoader.load_threaded_request(path)
+	var tree = get_tree()
+	var progress = 0
+	var arrProgress = [0]
+	while progress < 1:
+		await tree.process_frame
+		ResourceLoader.load_threaded_get_status(path, arrProgress)
+		progress = arrProgress[0]
+		scene_manager_progress.set_progress(progress)
+	return ResourceLoader.load_threaded_get(path)
+	
---
+++ scripts/scene_manager_progress.gd
@@ -0,0 +1,26 @@
+class_name SceneManagerProgress
+extends Control
+
+@onready var progress_container: Panel = $Panel/ProgressContainer
+@onready var progress_value: Panel = $Panel/ProgressContainer/ProgressValue
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var target_progress = 0.0
+
+func set_progress(value: float):
+	target_progress = value
+	if value == 0.0: 
+		progress_value.size.x = 0
+
+func _process(delta: float) -> void:
+		progress_value.size.x = lerp(progress_value.size.x, target_progress * progress_container.size.x, delta * 5)
+
+func appear():
+	animation_player.play("Appear")
+	await animation_player.animation_finished
+	print("finished")
+
+func disappear():
+	animation_player.play("Disappear")
+	await animation_player.animation_finished
+	print("finished")
